50f2a1b74c009fe5dded7ba0a374e7ee54e1ece6
xer34 <experimental@gmail.com>
2019-03-03 23:19:06 -0500

    native practice

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,30 +8,109 @@ import {
   TouchableOpacity
 } from "react-native";
 
+import Note from "./Note";
+
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBocEFG1hxeBFkDwZP587L1APyZino7Ec",
+  authDomain: "battletimer-24bb3.firebaseapp.com",
+  databaseURL: "https://battletimer-24bb3.firebaseio.com",
+  projectId: "battletimer-24bb3",
+  storageBucket: "battletimer-24bb3.appspot.com"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: ""
+    };
+
+    this.itemsRef = this.getRef().child("items");
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  componentWillMount() {
+    this.getItems(this.itemsRef);
+  }
+
+  componentDidMount() {
+    // this.getItems(this.itemsRef);
+  }
+
+  getItems(itemsRef) {
+    itemsRef.on("value", snap => {
+      let items = [];
+      snap.forEach(child => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+      this.setState({
+        itemDatSrouce: this.state.itemDatSource.cloneWithRows(items)
+      });
+    });
+  }
+
   render() {
+    let notes = this.state.noteArray.map((val, key) => {
+      return (
+        <Note
+          key={key}
+          keyval={key}
+          val={val}
+          deleteMethod={() => this.deleteNote(key)}
+        />
+      );
+    });
+
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.headerText}>BattleTimer</Text>
         </View>
 
-        <ScrollView style={styles.scrollContainer} />
+        <ScrollView style={styles.scrollContainer}> {notes}</ScrollView>
 
         <View style={styles.footer}>
           <TextInput
             style={styles.textInput}
+            onChangeText={noteText => this.setState({ noteText })}
+            value={this.state.noteText}
             placeholder="Enter Text Here"
             placeholderTextColor="white"
             underlineColorAndroid="transparent"
           />
         </View>
 
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity
+          onPress={this.addNote.bind(this)}
+          style={styles.addButton}
+        >
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
       </View>
     );
+  }
+
+  addNote() {
+    if (this.state.noteText) {
+      this.setState({ noteArray: this.state.noteArray });
+      this.setState({ noteText: "" });
+    }
+  }
+
+  deleteNote(key) {
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray });
   }
 }
 
